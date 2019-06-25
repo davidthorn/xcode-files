6f2777ec4a27786dc9f724d22515e2e65a943a1a
David Thorn <david.thorn221278@googlemail.com>
2019-06-25 18:57:13 +0200

    added example project

---
+++ ExampleProject/ExampleProject/ExampleProject.h
@@ -0,0 +1,19 @@
+//
+//  ExampleProject.h
+//  ExampleProject
+//
+//  Created by David Thorn on 25.06.19.
+//  Copyright © 2019 David Thorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExampleProject.
+FOUNDATION_EXPORT double ExampleProjectVersionNumber;
+
+//! Project version string for ExampleProject.
+FOUNDATION_EXPORT const unsigned char ExampleProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleProject/PublicHeader.h>
+
+
